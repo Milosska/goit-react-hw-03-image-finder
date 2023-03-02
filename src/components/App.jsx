3e42de1f9cbc3a41918d53ec27fa8d18dd6bb1f5
Milosska <110258611+Milosska@users.noreply.github.com>
2023-03-02 17:11:43 +0200

    Searchbar component is done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
+import { Component } from 'react';
 import { GlobalStyles } from './GlobalStyles';
 import 'modern-normalize';
+import { Searchbar } from './Searchbar/Searchbar';
 
-export const App = () => {
-  return (
-    <div>
-      <GlobalStyles />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    query: '',
+  };
+
+  handleSubmit = ({ query }, { resetForm }) => {
+    this.setState({ query });
+    resetForm();
+  };
+
+  render() {
+    return (
+      <div>
+        <Searchbar onSubmit={this.handleSubmit} />
+        <GlobalStyles />
+      </div>
+    );
+  }
+}
